eb342aa1b2d1b231026ea797177c6d8baf23df49
ssongd-creater <sksk1440@gmail.com>
2021-08-10 23:23:32 +0900

    8/10 fullpage 구축 및 디자인 설계 중

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
-  $('#wrap').fullpage({
+  $('#fullpage').fullpage({
+    // licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
     autoScrolling: true,
-    scrollHorizontally: true,
+    scrollHorizontally: false,
+    lockAnchors:false,
     anchors: ['1st', '2nd', '3rd', '4rd'],
-    sectionColor:['#333','#666','#ccc','#efefef'],
+    //sectionColor:['#333333','#666666','#cccccc','#efefef'],
     navigation: true,
     navigationPosition: 'right',
     sectionSelector: '.section',
-    scrollingSpeed:700,
+    css3:true,
+    scrollingSpeed: 700,
+    fitToSection: true
   });
   $.fn.fullpage.setAllowScrolling(false);
 });
