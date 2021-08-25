cad91c3568bdc0fc72dd5046250775130711f82b
ssongd-creater <sksk1440@gmail.com>
2021-08-25 18:35:53 +0900

    8/25 레이아웃 변경중

--- include/header.php
+++ include/header.php
@@ -1,16 +1,29 @@
+<div class="gb_section"></div>
+
 <header>
   <div class="navigation">
     <div class="header_wrap center">
       <a href="/dy_pofol/#home">
         <div class="logo">
-        <!-- <em>Creater</em><br> -->
-        SsongD
+          <!-- <em>Creater</em><br> -->
+          SsongD
         </div>
       </a>
-      <div class="navi"><a href="/dy_pofol/#1st"><p>I'm</p></a>
-        <a href="/dy_pofol/#2nd"><p>Web</p></a>
-        <a href="/dy_pofol/#3rd"><p>PS & AI</p></a>
-        <a href="/dy_pofol/#4rd"><p>Contact</p></a>
+      <div class="navi"><a href="/dy_pofol/#1st">
+          <p>I'm</p>
+        </a>
+        <a href="/dy_pofol/#2nd">
+          <p>Web</p>
+        </a>
+        <a href="/dy_pofol/#3rd">
+          <p>PS & AI</p>
+        </a>
+        <a href="/dy_pofol/#4rd">
+          <p>PRODUCE</p>
+        </a>
+        <a href="/dy_pofol/#5th">
+          <p>Contact</p>
+        </a>
       </div>
       <div class="sub_navwrap">
         <div class="sub_navi off"><i class="fa fa-bars"></i></div>
@@ -19,17 +32,16 @@
             <li><a href="/dy_pofol/#1st">I'm</a></li>
             <li><a href="/dy_pofol/#2nd">Web</a></li>
             <li><a href="/dy_pofol/#3rd">PS & AI</a></li>
-            <li><a href="/dy_pofol/#4rd">Contact</a></li>
+            <li><a href="/dy_pofol/#4rd">PS & AI</a></li>
+            <li><a href="/dy_pofol/#5th">Contact</a></li>
           </ul>
         </div>
       </div>
-      
     </div>
-    
 </header>
-  <div class="modal_box">
-    <div class="modal_con">
-      <button class="modal_close"><i class="fa fa-times-circle"></i></button>
-      <div class="modal_imgbox"></div>
-    </div>
+<!-- <div class="modal_box">
+  <div class="modal_con">
+    <button class="modal_close"><i class="fa fa-times-circle"></i></button>
+    <div class="modal_imgbox"></div>
   </div>
+</div> -->
--- null_idx.php
+++ null_idx.php
@@ -5,9 +5,15 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Creater Ssongd</title>
+  <title>Creater SsongD</title>
   <!-- Font Awesome Link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Full Page Plugin Link -->
+  <link rel="stylesheet" href="/dy_pofol/fullpage_lib/jquery.fullpage.css">
+  <!-- Main Style Code Sheet -->
+  <link rel="stylesheet" href="/dy_pofol/css/style.css">
+  <!-- Media Style Code Sheet -->
+  <link rel="stylesheet" href="/dy_pofol/css/media.css">
   <!-- Google font Link -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,20 +25,16 @@
   <link
     href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Gowun+Dodum&family=Roboto:wght@100;300;400;500;700;900&display=swap"
     rel="stylesheet">
-  <!-- Full Page Plugin Link -->
-  <link rel="stylesheet" href="/dy_pofol/fullpage_lib/jquery.fullpage.css">
-  <!-- Main Style Code Sheet -->
-  <link rel="stylesheet" href="/dy_pofol/css/style.css">
 </head>
 
 <body>
 
   <?php include $_SERVER["DOCUMENT_ROOT"]."/dy_pofol/include/header.php";?>
+
   <div id="fullpage">
-    <!-- Home Section Html -->
-    <section class="home section" data-anchor="home">
+    <div class="section pgnum_1" data-anchor="home">
       <div class="center">
-        <div class="vertical_center">
+        <div class="main_contents">
           <div class="left_wing"><img src="/dy_pofol/img/pngwing.com.png" alt=""></div>
           <div class="main_txt">
             <div class="typing-txt">
@@ -52,102 +54,108 @@
           </div>
           <div class="right_wing"><img src="/dy_pofol/img/pngwing.com.png" alt=""></div>
         </div>
-      </div>
-    </section>
+        <!-- End of main_contents -->
 
-    <!-- Iam Section Html -->
-    <section class="iam section" data-anchor="1st">
+      </div>
+    </div>
+    <!-- End of 1st Section -->
+    <div class="section pgnum_2" data-anchor="1st">
       <div class="center">
-        <div class="iam_wrap">
-          <div class="main2_contents">
-            <div class="main_img">
-              <img src="/dy_pofol/img/dayoung.jpg" alt="">
-            </div>
+
+        <div class="main2_contents">
+          <div class="main_img">
+            <img src="/dy_pofol/img/dayoung.jpg" alt="">
           </div>
 
-          <div class="card_wrap">
-            <div class="main2_fronttxt">
-              <h2>MY PROFILE</h2>
-              <div class="detail_profile">
-                <p><i class="fa fa-asterisk"> </i> 이름 : 송다영(宋多英)</p>
-                <p><i class="fa fa-asterisk"> </i> 생년월일 : 1991년 11월 12일</p>
-                <p class="education">EDUCATION</p>
-                <p><i class="fa fa-asterisk"> </i>극동대학교 산업디자인학과 졸업(과대 및 학회장 활동)</p>
-                <p><i class="fa fa-asterisk"> </i>2021.04.22 - 2021.09.03 더조은아카데미</p>
-                <p><i class="fa fa-asterisk"> </i>스마트 웹디자인(UI/UX) 콘텐츠 개발(웹퍼블리셔)_스마트</p>
-                <span>
-                  <h3><i class="fa fa-lightbulb-o"></i>성실함, 밝은, 노력하는<i class="fa fa-lightbulb-o"></i></h3>
-                </span>
-              </div>
-            </div>
-
-            <!-- main2_txt card back html -->
-            <div class="main2_backtxt">
-              <div class="main2_txt">
-                <h2>MY SKILL</h2>
-                <div class="skill_wrap">
-                  <ul class="left_skill">
-                    <li><i class="fa fa-asterisk"> </i> <em>HTML</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar html"></div>
-                      </div>
-                    </div>
-                    <li><i class="fa fa-asterisk"> </i><em> CSS</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar css"></div>
-                      </div>
-                    </div>
-                    <li><i class="fa fa-asterisk"> </i><em> JavaScript</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar javascript"></div>
-                      </div>
-                    </div>
-                    <li><i class="fa fa-asterisk"> </i><em> Jquery</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar jquery"></div>
-                      </div>
-                    </div>
-                  </ul>
-                  <ul class="right_skill">
-                    <li><i class="fa fa-asterisk"> </i><em> PHP</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar php"></div>
-                      </div>
-                    </div>
-                    <li><i class="fa fa-asterisk"> </i><em> PhotoShop</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar photoshop"></div>
-                      </div>
-                    </div>
-                    <li><i class="fa fa-asterisk"> </i><em> Illustrator</em></li>
-                    <div class="skill_bar">
-                      <div class="total_bar">
-                        <div class="sub_bar illustrator"></div>
-                      </div>
-                    </div>
-                  </ul>
+          <div class="container">
+            <div class="card_wrap">
+              <div class="main2_fronttxt">
+                <h2>MY PROFILE</h2>
+                <div class="detail_profile">
+                  <p><i class="fa fa-asterisk"> </i> 이름 : 송다영(宋多英)</p>
+                  <p><i class="fa fa-asterisk"> </i> 생년월일 : 1991년 11월 12일</p>
+                  <p class="education">EDUCATION</p>
+                  <p><i class="fa fa-asterisk"> </i>극동대학교 산업디자인학과 졸업(과대 및 학회장 활동)</p>
+                  <p><i class="fa fa-asterisk"> </i>2021.04.22 - 2021.09.03 더조은아카데미</p>
+                  <p><i class="fa fa-asterisk"> </i>스마트 웹디자인(UI/UX) 콘텐츠 개발(웹퍼블리셔)_스마트</p>
+                  <span>
+                    <h3><i class="fa fa-lightbulb-o"></i>성실함, 밝은, 노력하는<i class="fa fa-lightbulb-o"></i></h3>
+                  </span>
                 </div>
-                <!-- End of Skill_wrap Html -->
-                <p>* 워드, 파워포인트, 더존 사용 가능</p>
               </div>
-              <!-- End of Skill Html -->
+
+              <!-- main2_txt card back html -->
+              <div class="main2_backtxt">
+                <div class="main2_txt">
+                  <h2>MY SKILL</h2>
+                  <div class="skill_wrap">
+                    <ul class="left_skill">
+                      <li><i class="fa fa-asterisk"> </i> <em>HTML</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar html"></div>
+                        </div>
+                      </div>
+                      <li><i class="fa fa-asterisk"> </i><em> CSS</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar css"></div>
+                        </div>
+                      </div>
+                      <li><i class="fa fa-asterisk"> </i><em> JavaScript</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar javascript"></div>
+                        </div>
+                      </div>
+                      <li><i class="fa fa-asterisk"> </i><em> Jquery</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar jquery"></div>
+                        </div>
+                      </div>
+                    </ul>
+                    <ul class="right_skill">
+                      <li><i class="fa fa-asterisk"> </i><em> PHP</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar php"></div>
+                        </div>
+                      </div>
+                      <li><i class="fa fa-asterisk"> </i><em> PhotoShop</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar photoshop"></div>
+                        </div>
+                      </div>
+                      <li><i class="fa fa-asterisk"> </i><em> Illustrator</em></li>
+                      <div class="skill_bar">
+                        <div class="total_bar">
+                          <div class="sub_bar illustrator"></div>
+                        </div>
+                      </div>
+                    </ul>
+                  </div>
+                  <!-- End of Skill_wrap Html -->
+                  <p>* 워드, 파워포인트, 더존 사용 가능</p>
+                </div>
+                <!-- End of Skill Html -->
+              </div>
+
+            </div>
+            <div class="skill_btn">
+              <button>View My Skill</button>
             </div>
           </div>
+          <!-- End of Card Wrap Html -->
         </div>
-        <div class="skill_btn">
-          <button class="skill_flip_btn">View My Skill</button>
-        </div>
-      </div>
-    </section>
 
-    <!-- Web Portpolio Html -->
-    <section class="web section" data-anchor="2nd">
+        <!-- End of main2_section Html -->
+
+      </div>
+      <!-- End of Center Html -->
+    </div>
+    <div class="section pgnum_3" data-anchor="2nd">
       <div class="center">
         <div class="slide slide_1">
 
@@ -292,10 +300,8 @@
           </div>
         </div>
       </div>
-    </section>
-
-    <!-- PS & AI Porfolio Html  -->
-    <section class="psai section" data-anchor="3rd">
+    </div>
+    <div class="section pgnum_4" data-anchor="3rd">
       <div class="center">
         <div class="pgnum_4wrap">
           <div class="punum_4_mainbox">
@@ -358,6 +364,7 @@
                 <p> - 포토샵과 일러스트레이터를 활용하여 디자인한 작업물입니다.</p>
                 <p> - 해당 사진을 더 크게 보고싶다면 아래 버튼을 눌러주세요 :-)</p>
 
+
                 <button type="button" class="view_img">자세히 보기</button>
               </div>
             </div>
@@ -366,19 +373,18 @@
         </div>
       </div>
       <!-- End of Center -->
-    </section>
-    <section class="produce section" data-anchor="4rd">produce</section>
-    <section class="contact section" data-anchor="5th">contact</section>
+    </div>
+
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/dy_pofol/pages/community_form.php"; ?>
+
+  </div>
 
 
 
-
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="/dy_pofol/js/submain.js"></script>
-    <script src="/dy_pofol/fullpage_lib/jquery.fullpage.js"></script>
-    <script src="/dy_pofol/fullpage_lib/jquery.fullpage.extensions.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="/dy_pofol/js/main.js"></script>
+  <script src="/dy_pofol/fullpage_lib/jquery.fullpage.js"></script>
+  <script src="/dy_pofol/fullpage_lib/jquery.fullpage.extensions.min.js"></script>
 </body>
 
 </html>
